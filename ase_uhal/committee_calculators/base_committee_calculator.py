Fix HAL score calculation

ase_uhal/committee_calculators/base_committee_calculator.py
@@ -231,28 +231,24 @@ class BaseCommitteeCalculator(Calculator, metaclass=ABCMeta):
         '''
         super().calculate(atoms, properties, system_changes)
 
-        results = {}
-
         if "energy" in properties or "forces" in properties:
             Es = self.get_committee_energies(atoms)
 
         if "energy" in properties:
-            results["energy"] = np.std(Es)
+            self.results["hal_energy"] = np.std(Es)
 
         if "forces" in properties:
             Es -= np.mean(Es)
             Fs = self.get_committee_forces(atoms)
             Fs -= np.mean(Fs, axis=0)
 
-            results["forces"] = np.mean([E * F for E, F in zip(Es, Fs)], axis=0)
+            self.results["hal_forces"] = np.mean([E * F for E, F in zip(Es, Fs)], axis=0)
 
         if "stress" in properties:
             Ss = self.get_committee_stresses(atoms)
             Ss -= np.mean(Ss, axis=0)
 
-            results["stresses"] = np.mean([E * S for E, S in zip(Es, Ss)], axis=0)
-        
-        return results
+            self.results["hal_stresses"] = np.mean([E * S for E, S in zip(Es, Ss)], axis=0)
 
     def __update_likelihood_core(self, atoms, energy_weight, force_weight, stress_weight):
         l = {}

ase_uhal/committee_calculators/mace_committee_calculator.py
@@ -242,8 +242,6 @@ class MACECommitteeCalculator(BaseCommitteeCalculator):
         '''
         Calculator.calculate(self, atoms, properties, system_changes)
 
-        results = {}
-
         if atoms is None:
             atoms = self.atoms
 
@@ -251,12 +249,10 @@ class MACECommitteeCalculator(BaseCommitteeCalculator):
 
         E_hal = self._hal_energy(*props)
 
-        results["energy"] = E_hal.detach().cpu().numpy()
+        self.results["hal_energy"] = E_hal.detach().cpu().numpy()
 
         if "forces" in properties:
             # derivative w.r.t positions
             grad_outputs = [torch.ones_like(E_hal)]
             Fs = torch.autograd.grad(outputs=[E_hal], inputs=[props[0]], grad_outputs=grad_outputs, allow_unused=True)[0]
-            results["forces"] = Fs.detach().cpu().numpy()
-
-        return results
+            self.results["hal_forces"] = Fs.detach().cpu().numpy()

ase_uhal/hal_calculator.py
@@ -60,13 +60,13 @@ class HALCalculator(Calculator):
 
         assert self.tau is not None
 
-        committee_results = self.committee_calc.hal_calculate(atoms, properties, system_changes)
+        self.committee_calc.hal_calculate(atoms, properties, system_changes)
 
         self.mean_calc.calculate(atoms, properties, system_changes)
 
         for prop in self.implemented_properties:
             if prop in properties:
-                self.results[prop] = self.mean_calc.results[prop] - self.tau * committee_results[prop]
+                self.results[prop] = self.mean_calc.results[prop] - self.tau * self.committee_calc.results["hal_" + prop]
 
     def update_tau(self, atoms=None):
         '''
@@ -78,9 +78,8 @@ class HALCalculator(Calculator):
         Modifies self.tau if self.tau_delay < 0, otherwise decreases self.tau_delay by 1
         '''
         self.get_property("forces", atoms)
-        committee_results = self.committee_calc.hal_calculate(atoms, "forces")
-        Fmean = np.mean(np.linalg.norm(self.mean_calc.properties["forces"], axis=-1))
-        Fbias = np.mean(np.linalg.norm(committee_results["forces"], axis=-1))
+        Fmean = np.mean(np.linalg.norm(self.mean_calc.results["forces"], axis=-1))
+        Fbias = np.mean(np.linalg.norm(self.committee_calc.results["hal_forces"], axis=-1))
 
         if self.Fmean is None or self.Fbias is None:
             self.Fmean = Fmean
@@ -99,9 +98,11 @@ class HALCalculator(Calculator):
 
     def get_hal_score(self, atoms=None):
         self.get_property("forces", atoms)
+        Fmean = np.linalg.norm(self.mean_calc.results["forces"], axis=-1)
+        Fbias = np.linalg.norm(self.committee_calc.results["hal_forces"], axis=-1)
         
-        F = np.linalg.norm(self.committee_calculator.results["forces"], axis=-1) / (np.linalg.norm(self.mean_calc.results["forces"], axis=-1) + self.eps)
+        F = Fbias / (Fmean + self.eps)
 
         s = np.exp(F) / np.sum(np.exp(F)) # Apply softmax
 
-        return s
+        return np.max(s)