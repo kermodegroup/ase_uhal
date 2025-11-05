4bd09d6bffe4fe261f35c1bb45916fa7e0a2aca0
Thomas Rocke <45517493+thomas-rocke@users.noreply.github.com>
2025-11-05 18:02:24 +0000

    Add StructureSelector

--- ase_uhal/__init__.py
+++ ase_uhal/__init__.py
@@ -1,7 +1,7 @@
 """Implementation of "Universal HyperActive Learning" compatible with the Atomic Simulation Environment (ASE)"""
 
 # Add imports here
-from .hal_calculator import HALCalculator
+from .hal_calculator import HALCalculator, StructureSelector
 from .committee_calculators import ACECommitteeCalculator, MACECommitteeCalculator
 
 from ._version import __version__
